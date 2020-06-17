8d1705a15e392870e5137aed729685d755734ab2
mesutBE <gelukkig.zijn.be@gmail.com>
2020-06-17 21:54:57 +0200

    Setup and v1

---
+++ src/components/Titles.js
@@ -0,0 +1,26 @@
+
+import React from "react";
+
+const Titles = () => (
+    <div>
+        <h1 className="title-container__title">Weather Finder</h1>
+        <h3 className="title-container__subtitle">Find out temperature, conditions and more...</h3>
+    </div>
+);
+
+export default Titles;
+
+// import React from "react";
+
+// class Titles extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <h1>Weather Finder</h1>
+//                 <p>Hello!</p>
+//             </div>
+//         );
+//     }
+// }
+
+// export default Titles;
